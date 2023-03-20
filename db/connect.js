a8ea61261075d31fcef5f69cb24406fc17c5e754
mahmoud hamdy <72884628+m-hamdy1572@users.noreply.github.com>
2023-03-20 20:24:32 +0200

    remove strUri mongo

--- db/connect.js
+++ db/connect.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-// const connectionString = "mongodb+srv://mh15721812:15721812@cluster0.owthrxo.mongodb.net/TaskManger?retryWrites=true&w=majority";
+
 
 const connectDB = (uri) => {
     return mongoose
